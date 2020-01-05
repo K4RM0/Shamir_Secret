88798c960ee0712d5de48198ceacba66725e5a21
draguarMatth <50259605+draguarMatth@users.noreply.github.com>
2020-01-05 18:56:50 +0100

    Test Matthieu

---
+++ PartieMatthieuEclipse/src/main/java/ExceptionShamirSecret.java
@@ -0,0 +1,9 @@
+
+/**
+ * <p> Exception lorsqu'on calcule le secret </p>
+ */
+public class ExceptionShamirSecret extends Exception {
+    public ExceptionShamirSecret(String msg) {
+        super(msg);
+    }
+}
